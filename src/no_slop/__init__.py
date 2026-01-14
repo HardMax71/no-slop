1d59418f2f23ae847e39bc96fac169865e4bda88
HardMax71 <maxymazatyan@gmail.com>
2026-01-14 17:06:18 +0100

    preflight checks before running tests

--- src/no_slop/__init__.py
+++ src/no_slop/__init__.py
@@ -1,5 +1,4 @@
 from no_slop._version import __version__
-
 from no_slop.flake8_plugin import SlopStyleChecker
 from no_slop.mypy_plugin import NoSlopPlugin
 from no_slop.unused_defaults import UnusedDefaultsChecker
